866f5a80582bb47ae4d7461de1e782ff04acffa0
JakubOlejnik <jacobole2000@gmail.com>
2024-02-13 08:43:42 +0100

    Small fixes for layout and validation of input

--- app/src/main/java/com/example/gra_smok/MainActivity.java
+++ app/src/main/java/com/example/gra_smok/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gra_smok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button playButton = findViewById(R.id.playButton);
+        EditText numberInput = findViewById(R.id.numberInput);
 
+        playButton.setOnClickListener(view -> {
+            String numberInputValue = numberInput.getText().toString().trim();
+            String message;
+            if(!checkIsInputValid(numberInputValue)) {
+                message = "Wprowadzone dane są błędne! Liczba w polu powinna znajdować się w przedziale od 0 do 100";
+            } else {
+
+            }
+
+        });
+
+    }
+
+    private boolean checkIsInputValid(String inputValue) {
+        if (inputValue.isEmpty()) return false;
+
+        int valueFromInput = Integer.parseInt(inputValue);
+
+        if (valueFromInput < 0 || valueFromInput > 100) return false;
+        return true;
 
     }
 }
