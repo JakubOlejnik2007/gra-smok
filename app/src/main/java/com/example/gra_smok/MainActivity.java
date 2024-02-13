37d8ed3f084660935cdd16d3e5aac5d6159722d2
JakubOlejnik <jacobole2000@gmail.com>
2024-02-13 13:48:33 +0100

    Documentation for methods in MainActivity.java

--- app/src/main/java/com/example/gra_smok/MainActivity.java
+++ app/src/main/java/com/example/gra_smok/MainActivity.java
@@ -12,6 +12,15 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The onCreate function sets up the main activity layout and handles the button click event to
+     * check if the user has won against a dragon based on the input number.
+     * 
+     * @param savedInstanceState The savedInstanceState parameter is a Bundle object that contains the
+     * data saved from the previous state of the activity. It is used to restore the activity's
+     * previous state, such as when the activity is recreated after being destroyed and re-created by
+     * the system.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,11 +61,24 @@ public class MainActivity extends AppCompatActivity {
         return valueFromInput >= 0 && valueFromInput <= 100;
     }
 
+    /**
+     * The function checks if the given value is equal to a randomly generated number between 0 and 99.
+     * 
+     * @param value The value parameter represents the user's guess.
+     * @return The method is returning a boolean value.
+     */
     private boolean checkIfUserWon(int value) {
         Random random = new Random();
         return value == random.nextInt(100);
     }
 
+    /**
+     * The function "handleDialog" creates and displays an alert dialog with a given message and a
+     * positive button.
+     * 
+     * @param message The message parameter is a string that represents the content of the dialog
+     * message. It is the text that will be displayed to the user in the dialog box.
+     */
     private void handleDialog(String message) {
         AlertDialog.Builder builder = new
                 AlertDialog.Builder(MainActivity.this);
